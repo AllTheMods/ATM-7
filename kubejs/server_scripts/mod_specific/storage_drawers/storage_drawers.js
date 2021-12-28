c403f1c77863cc1bb7ad3e3def6c426f7e69c77b
Unknown <mattmaryanski@gmail.com>
2021-12-28 16:02:52 -0500

    Add Creative Storage Recipe
    still no atm star yet

---
+++ kubejs/server_scripts/mod_specific/storage_drawers/storage_drawers.js
@@ -0,0 +1,7 @@
+onEvent('recipes', event => {
+   event.shaped('16x storagedrawers:creative_storage_upgrade', ['EEE', 'ASA', 'EEE'], {
+    E: 'storagedrawers:emerald_storage_upgrade',
+    S: 'allthetweaks:atm_star',
+    A: 'allthemodium:allthemodium_block'
+  })
+})
