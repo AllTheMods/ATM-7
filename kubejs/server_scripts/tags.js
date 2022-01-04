ca34ee380f12dcf491d5f5ba41e8b7d71f232960
Jeremiah Winsley <jeremiah.winsley@gmail.com>
2022-01-03 23:59:22 -0500

    Add MA support to IE Garden Cloche

---
+++ kubejs/server_scripts/tags.js
@@ -0,0 +1,9 @@
+//priority: 1000
+onEvent('tags.items', e => {
+    e.add('misctags:farmland/tier6', ['mysticalagradditions:insanium_farmland'])
+    e.add('misctags:farmland/tier5', ['mysticalagriculture:supremium_farmland', '#misctags:farmland/tier6'])
+    e.add('misctags:farmland/tier4', ['mysticalagriculture:imperium_farmland', '#misctags:farmland/tier5'])
+    e.add('misctags:farmland/tier3', ['mysticalagriculture:tertium_farmland', '#misctags:farmland/tier4'])
+    e.add('misctags:farmland/tier2', ['mysticalagriculture:prudentium_farmland', '#misctags:farmland/tier3'])
+    e.add('misctags:farmland/tier1', ['mysticalagriculture:inferium_farmland', '#misctags:farmland/tier2'])
+})
