889b9ce27fa83104a2f63a4d0b5985d1401f67c2
Jeremiah Winsley <jeremiah.winsley@gmail.com>
2022-04-04 00:35:13 -0400

    Update MA integration

--- kubejs/server_scripts/constants.js
+++ kubejs/server_scripts/constants.js
@@ -1,5 +1,7 @@
 //priority: 1000
 let colors = ['white', 'orange', 'magenta', 'light_blue', 'yellow', 'lime', 'pink', 'gray', 'light_gray', 'cyan', 'purple', 'blue', 'brown', 'green', 'red', 'black']
+let maDisabledSeeds = ['apatite', 'rubber', 'ruby', 'sapphire', 'basalz', 'blazing_crystal', 'blitz', 'blizz', 'brass', 'bronze', 'compressed_iron', 'constantan', 'chrome', 'electrum', 'elementium', 'ender_biotite', 'energized_steel', 'fluix', 'graphite', 'hop_graphite', 'invar', 'iridium', 'manasteel', 'niotic_crystal', 'nitro_crystal', 'quartz_enriched_iron', 'refined_glowstone', 'refined_obsidian', 'rock_crystal', 'rubber', 'signalum', 'silicon', 'sky_stone', 'spirited_crystal', 'starmetal', 'steel', 'sulfur', 'terrasteel', 'titanium', 'tungsten', 'mithril', 'tinkers_bronze', 'ironwood', 'steeleaf', 'pig_iron', 'slimesteel', 'rose_gold', 'queens_slime', 'manyullyn', 'fiery_ingot', 'knightmetal', 'hepatizon', 'lumium', 'enderium']
+
 let draconicFusion = (e, output, craftingTier, energy, middleItem, ingredientList) => {
   //crafting tier: 1.draconium, 2.wyvern, 3.draconic, 4.chaotic
   let tiers = ['WYVERN', 'DRACONIC', 'CHAOTIC']
---
+++ kubejs/server_scripts/mod_specific/mystical_agriculture/ma_crafting.js
@@ -0,0 +1,55 @@
+onEvent('recipes', e => {
+  maDisabledSeeds.forEach(name => {
+    ['seeds', 'essence'].forEach(type => {
+      e.remove({ output: `mysticalagriculture:${name}_${type}` })
+      e.remove({ input: `mysticalagriculture:${name}_${type}` })
+    });
+  });
+
+  e.remove({ id: 'mysticalagriculture:augment/flight' });
+  maInfusion(e, 'mysticalagriculture:flight_augment', 'mysticalagriculture:unattuned_augment', 'mysticalagriculture:supremium_block', 'mysticalagriculture:supremium_block', 'mysticalagriculture:supremium_block', 'allthemodium:unobtainium_block', 'allthecompressed:nether_star_block_1x', 'mysticalagriculture:supremium_block', 'allthecompressed:nether_star_block_1x', 'allthemodium:unobtainium_block', 'mysticalagriculture:supremium_block')
+
+  let fish = [
+    'aquaculture:arapaima',
+    'aquaculture:atlantic_cod',
+    'aquaculture:atlantic_halibut',
+    'aquaculture:atlantic_herring',
+    'aquaculture:bayad',
+    'aquaculture:blackfish',
+    'aquaculture:bluegill',
+    'aquaculture:boulti',
+    'aquaculture:brown_shrooma',
+    'aquaculture:brown_trout',
+    'aquaculture:capitaine',
+    'aquaculture:carp',
+    'aquaculture:catfish',
+    'aquaculture:gar',
+    'aquaculture:minnow',
+    'aquaculture:muskellunge',
+    'aquaculture:pacific_halibut',
+    'aquaculture:perch',
+    'aquaculture:pink_salmon',
+    'aquaculture:piranha',
+    'aquaculture:pollock',
+    'aquaculture:rainbow_trout',
+    'aquaculture:red_grouper',
+    'aquaculture:red_shrooma',
+    'aquaculture:smallmouth_bass',
+    'aquaculture:synodontis',
+    'aquaculture:tambaqui',
+    'aquaculture:tuna',
+  ];
+
+  fish.forEach(name => {
+    e.custom({
+      type: 'mysticalagriculture:soul_extraction',
+      input: {
+        item: name
+      },
+      output: {
+        type: 'mysticalagriculture:fish',
+        souls: 0.5
+      }
+    })
+  })
+})
