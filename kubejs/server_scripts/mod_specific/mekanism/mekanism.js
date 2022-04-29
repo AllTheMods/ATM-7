28a9465921ee971197ba1cb5d21615a62760b9f2
Jeremiah Winsley <jeremiah.winsley@gmail.com>
2022-04-28 21:35:03 -0400

    recipe fixes and config change

--- kubejs/server_scripts/mod_specific/mekanism/mekanism.js
+++ kubejs/server_scripts/mod_specific/mekanism/mekanism.js
@@ -1,10 +1,4 @@
 onEvent('recipes', e => {
-  e.shaped('mekanism:creative_energy_cube', ['ATA', 'VCV', 'ATA'], {
-    A: 'mekanism:alloy_atomic',
-    T: 'mekanism:energy_tablet',
-    V: 'allthemodium:vibranium_block',
-    C: 'mekanism:ultimate_energy_cube'
-  }).id('kubejs:mekanism/creative_energy_cube')
   e.shaped('mekanism:digital_miner', ['AUA', 'LRL', 'TST'], {
     L: 'mekanism:logistical_sorter',
     R: 'mekanism:robit',
@@ -19,13 +13,6 @@ onEvent('recipes', e => {
     U: '#forge:circuits/ultimate',
     V: '#forge:ingots/vibranium'
   }).id(`kubejs:mekanism/atomic_disassembler`)
-  e.shaped('mekanism:chemical_oxidizer', ['RCR', 'PDT', 'RCR'], {
-    R: 'mekanism:alloy_infused',
-    C: 'mekanism:basic_control_circuit',
-    P: 'mekanism:personal_chest',
-    D: 'mekanism:dynamic_tank',
-    T: 'mekanism:basic_chemical_tank'
-  }).id(`kubejs:mekanism/chemical_oxidizer`)
   e.shaped('mekanism:robit', [' S ', 'ERE', 'OPO'], {
     R: 'mekanism:alloy_atomic',
     S: '#forge:ingots/steel',
@@ -33,32 +20,28 @@ onEvent('recipes', e => {
     O: 'mekanism:ingot_refined_obsidian',
     P: 'mekanism:personal_chest'
   }).id(`kubejs:mekanism/robit`)
-  e.shaped('mekanism:laser_tractor_beam', ['P', 'L'], {
-    P: 'mekanism:personal_chest',
-    L: 'mekanism:laser_amplifier'
-  }).id(`kubejs:mekanism/laser_tractor_beam`)
-  e.shaped(Item.of('mekanism:mekasuit_helmet', { HideFlags: 2 }), ['HCH', 'HUH', 'PIP'], {
+  e.shaped(Item.of('mekanism:mekasuit_helmet', {HideFlags: 2}), ['HCH', 'HUH', 'PIP'], {
     H: 'mekanism:hdpe_sheet',
     C: 'mekanism:ultimate_control_circuit',
     P: 'mekanism:pellet_polonium',
     I: 'mekanism:basic_induction_cell',
     U: 'allthemodium:unobtainium_helmet'
   }).id(`kubejs:mekanism/mekasuit_helmet`)
-  e.shaped(Item.of('mekanism:mekasuit_bodyarmor', { HideFlags: 2 }), ['HCH', 'HUH', 'PIP'], {
+  e.shaped(Item.of('mekanism:mekasuit_bodyarmor', {HideFlags: 2}), ['HCH', 'HUH', 'PIP'], {
     H: 'mekanism:hdpe_sheet',
     C: 'mekanism:ultimate_control_circuit',
     P: 'mekanism:pellet_polonium',
     I: 'mekanism:basic_induction_cell',
     U: 'allthemodium:unobtainium_chestplate'
   }).id(`kubejs:mekanism/mekasuit_bodyarmor`)
-  e.shaped(Item.of('mekanism:mekasuit_pants', { HideFlags: 2 }), ['HCH', 'HUH', 'PIP'], {
+  e.shaped(Item.of('mekanism:mekasuit_pants', {HideFlags: 2}), ['HCH', 'HUH', 'PIP'], {
     H: 'mekanism:hdpe_sheet',
     C: 'mekanism:ultimate_control_circuit',
     P: 'mekanism:pellet_polonium',
     I: 'mekanism:basic_induction_cell',
     U: 'allthemodium:unobtainium_leggings'
   }).id(`kubejs:mekanism/mekasuit_pants`)
-  e.shaped(Item.of('mekanism:mekasuit_boots', { HideFlags: 2 }), ['HCH', 'HUH', 'PIP'], {
+  e.shaped(Item.of('mekanism:mekasuit_boots', {HideFlags: 2}), ['HCH', 'HUH', 'PIP'], {
     H: 'mekanism:hdpe_sheet',
     C: 'mekanism:ultimate_control_circuit',
     P: 'mekanism:pellet_polonium',
@@ -66,24 +49,14 @@ onEvent('recipes', e => {
     U: 'allthemodium:unobtainium_boots'
   }).id(`kubejs:mekanism/mekasuit_boots`)
 
-  mekanismMetals.forEach(metal => {
-    e.remove({id: `mekanism:processing/${metal}/ore/from_dust`})
-  })
-
   removeRecipeByID(e, [
-    /^mekanism:processing\/.*\/storage_blocks\/from_ingots/,
     'mekanism:digital_miner',
     'mekanism:atomic_disassembler',
     'mekanism:mekasuit_helmet',
     'mekanism:mekasuit_bodyarmor',
     'mekanism:mekasuit_pants',
     'mekanism:mekasuit_boots',
-    'mekanism:processing/diamond/to_dust',
-    'mekanism:gas_conversion/salt_to_hydrogen_chloride',
-    'mekanism:chemical_oxidizer',
     'mekanism:robit',
-    'mekanism:laser_tractor_beam'
-
   ])
   removeRecipeByOutput(e, [
     'mekanism:upgrade_anchor'
