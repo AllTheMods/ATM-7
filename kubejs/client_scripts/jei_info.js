6ff9c73b221f63e6cd7dd6a406d8c5d259836845
Mitchell5200 <45913837+Mitchell5200@users.noreply.github.com>
2022-07-04 18:02:14 +0800

    Update jei_info.js

--- kubejs/client_scripts/jei_info.js
+++ kubejs/client_scripts/jei_info.js
@@ -1,11 +1,6 @@
 onEvent('jei.information', e => {
 
   e.add('thermal:bitumen', ['Byproduct of refining Oil in a Distillation Tower'])
-  e.add('mekanism:creative_energy_cube', ['To fill the Cube you have to get 2x Ultimate Induction Providers, 2x filled Ultimate Energy Cubes and an ATM Star.',
-    '',
-    'You must build a multiblock structure in a horizontal line by placing the Creative Cube down with the Providers on either sides and the Ultimate Cubes at the end.',
-    '',
-    'All you have to do now, is to make sure your cubes are filled, after which you can Shift + RClick the Creative Cube with the Star.'])
 
   e.add('enderchests:ender_chest', [
     'Cross-dimensional wireless item transfer to any chest on the same channel.',
