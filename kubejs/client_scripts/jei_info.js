5bdf25da6c2636dbcd413f02767f1cea69387d26
Mitchell5200 <45913837+Mitchell5200@users.noreply.github.com>
2022-07-22 22:44:53 +0800

    Update jei_info.js
    Add Jei info tab for the Mek Creative cube

--- kubejs/client_scripts/jei_info.js
+++ kubejs/client_scripts/jei_info.js
@@ -15,4 +15,10 @@ onEvent('jei.information', e => {
     'Sneak + right-click with a diamond to switch to private channel.',
     'Sneak + right-click with an ender pearl or eye of ender to increase tank size.',
   ])
+  
+    e.add('mekanism:creative_energy_cube',[
+    'Needs to be energized in a Powah energizing rod with a ATM star.',
+    'Otherwise acts as an expensive energy trash can.',
+  ])
+  
 })
