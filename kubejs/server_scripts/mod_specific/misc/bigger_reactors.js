d24ba58def13e46a9135210f8542c897fb872293
Mitchell5200 <45913837+Mitchell5200@users.noreply.github.com>
2022-09-03 11:00:10 +0800

    Add files via upload
    Added Temp craft for ludicrite block to now use `#forge:storage_blocks/enderium`

---
+++ kubejs/server_scripts/mod_specific/misc/bigger_reactors.js
@@ -0,0 +1,10 @@
+onEvent('recipes', e=>{
+    ///Temp Fix for ludicrite block
+    e.shaped('biggerreactors:ludicrite_block',['BRB','E E','BRB'],{
+        B: 'biggerreactors:blutonium_ingot',
+        R: 'minecraft:blaze_rod',
+        E: '#forge:storage_blocks/enderium',
+    })
+
+
+})
